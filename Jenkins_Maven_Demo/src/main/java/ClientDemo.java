f8472630ca62f161b90a838d5bf453ededf688a1
mande <mande@DESKTOP-E5G2QQT>
2021-05-18 10:23:52 +0530

    ClientDemo

--- Jenkins_Maven_Demo/src/main/java/ClientDemo.java
+++ Jenkins_Maven_Demo/src/main/java/ClientDemo.java
@@ -5,8 +5,8 @@ public class ClientDemo {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		Demo d=new Demo();
-		System.out.println("multiplication is:-"+d.add(10, 10));
+		Demo d = new Demo();
+		System.out.println("multiplication is:-" + d.add(10, 10));
 	}
 
 }
